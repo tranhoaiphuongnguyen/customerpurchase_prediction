7eda60759f730d39b26362e4be694eb3dba1d5f0
Tran Hoai Phuong Nguyen <hoaiphuong.charlotte@gmail.com>
2025-11-27 19:44:18 +0000

    Update code.py
    Removed metadata and comments from the code, retaining only the necessary code for analysis.

--- code.py
+++ code.py
@@ -1,35 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Converted from Jupyter Notebook: notebook.ipynb
-Conversion Date: 2025-11-26T01:06:31.698Z
-"""
-
-# Online shopping decisions rely on how consumers engage with online store content. You work for a new startup company that has just launched a new online shopping website. The marketing team asks you, a new data scientist, to review a dataset of online shoppers' purchasing intentions gathered over the last year. Specifically, the team wants you to generate some insights into customer browsing behaviors in November and December, the busiest months for shoppers. You have decided to identify two groups of customers: those with a low purchase rate and returning customers. After identifying these groups, you want to determine the probability that any of these customers will make a purchase in a new marketing campaign to help gauge potential success for next year's sales.
-# 
-# ### Data description:
-# 
-# `online_shopping_session_data.csv` that contains several columns about each shopping session. Each shopping session corresponded to a single user. 
-# 
-# |Column|Description|
-# |--------|-----------|
-# |`SessionID`|unique session ID|
-# |`Administrative`|number of pages visited related to the customer account|
-# |`Administrative_Duration`|total amount of time spent (in seconds) on administrative pages|
-# |`Informational`|number of pages visited related to the website and the company|
-# |`Informational_Duration`|total amount of time spent (in seconds) on informational pages|
-# |`ProductRelated`|number of pages visited related to available products|
-# |`ProductRelated_Duration`|total amount of time spent (in seconds) on product-related pages|
-# |`BounceRates`|average bounce rate of pages visited by the customer|
-# |`ExitRates`|average exit rate of pages visited by the customer|
-# |`PageValues`|average page value of pages visited by the customer|
-# |`SpecialDay`|closeness of the site visiting time to a specific special day|
-# |`Weekend`|indicator whether the session is on a weekend|
-# |`Month`|month of the session date|
-# |`CustomerType`|customer type|
-# |`Purchase`|class label whether the customer make a purchase|
-
-
 # Import required libraries
 import pandas as pd
 import numpy as np
@@ -42,7 +10,6 @@ shopping_data = pd.read_csv("online_shopping_session_data.csv")
 shopping_data.head()
 
 # ### Objective: calculate the percentage of customers by customer type, for November–December only
-
 
 # Filter data for November and December
 NovDec_data = shopping_data[shopping_data["Month"].isin(["Nov", "Dec"])]
@@ -73,9 +40,7 @@ plt.show()
 
 # ________
 
-
 # ### Objective: for returning customers, find the 2 duration variables (*_Duration) that have the highest correlation
-
 
 # Filter returning customers in Nov & Dec
 returning = NovDec_data[NovDec_data["CustomerType"] == "Returning_Customer"]
@@ -105,10 +70,8 @@ plt.show()
 
 # ______
 
-
 # **Let p be the purchase rate of previous returning customers. New campaign increases by 15% → p_new = p * 1.15.**
 # ### Objective: use binomial distribution to calculate the probability of having at least 100 orders in 500 sessions
-
 
 # Original purchase rate for returning customers
 p_original = purchase_rates['Returning_Customer']
